4082431c8e3a90d75df2f26f7ea6dff2a72a11ff
Taeyang Oh <sunio00000@naver.com>
2021-03-22 01:05:22 +0900

    feat: Add Asset, Buy, Sell socket trans - 소켓 통신 기본 예제 구현 - 매도/매수, 자산 갱신

--- GameDJ/sockets.js
+++ GameDJ/sockets.js
@@ -32,11 +32,37 @@ export default {
                 console.log(comment);
             });
             /////////////////////////////////////////
+            //@ buy / sell interaction
+            let myCash = 100000;
+            const refresh = (data) => {
+                console.log(socket.id, '님의 자산이 갱신되었습니다.')
+                socket.emit('asset', {
+                    cash: data.cash,
+                    asset: data.asset,
+                })
+            }
+            socket.on('buy', (data) => {
+                console.log(socket.id, '님의 [ 가격', data.currentBid,', 갯수', data.currentVolume ,'] 매수 주문이 체결되었습니다.')
+                socket.emit('buy', 3000);
+                refresh({
+                    cash: myCash - data.currentBid * data.currentVolume,
+                    asset: myCash,
+                })
+            });
+            socket.on('sell', (data) => {
+                console.log(socket.id, '님의 [ 가격', data.currentBid,', 갯수', data.currentVolume ,'] 매도 주문이 체결되었습니다.');
+                socket.emit('sell', 1000000);
+                refresh({
+                    cash: myCash + data.currentBid * data.currentVolume,
+                    asset: myCash,
+                })
+            })
+            /////////////////////////////////////////
             console.log('connected user');
             socket.on('createPrivateRoom_Req', (profile) => new Room(io, socket).createPrivateRoom(profile));
             socket.on('joinRoom_Req', async (joinData) => { await new Room(io, socket).joinRoom(joinData) });
             socket.on('startGame_Req', async () => await new Game(io, socket).startGame());
-            socket.on('');
+            // socket.on('-');
         });
     }
 }
