52f0655a9dc148191441d051779a0c5373dc4e73
syeeuns <syeeuns@gmail.com>
2021-03-21 23:54:07 +0900

    Add music setting (server)

--- GameDJ/gameDJ/game.js
+++ GameDJ/gameDJ/game.js
@@ -16,9 +16,10 @@ class Game {
 
     async startGame() {
         const { io, socket } = this;
+        games[socket.roomID][gameTime] = music_time;
         const { gameTime } = games[socket.roomID];
         const players = Array.from(await io.in(socket.roomID).allSockets());
-        socket.to(socket.roomID).emit('startGame', (gameTime));
+        socket.to(socket.roomID).emit('startGame_Res', (gameTime));
 
 
 
@@ -38,15 +39,15 @@ class Game {
     async renewalCurCoin() {
         const { io, socket } = this;
         const { gameTime } = games[socket.roomID];
-        
-        
+
+
         // 1. bidList 불러옴
         let curCoin = await dbget("curCoin");
         console.log(curCoin);
         socket.to(socket.roomID).emit('renewalCurCoin', curCoin);
         curPrice = Number(curCoin["curPrice"]);
-        
-    
+
+
         // 2. prePrice랑 curPrice를 비교
         // 2-1. curPrice === prePrice면 아무것도 하지않는다.
         // 2-2. curPrice >= prePrice면, askPrice에서 curPrice보다 낮은 호가를 처리한다.
@@ -56,7 +57,7 @@ class Game {
             for (let askPrice in askList) {
                 let intAskPrice = Number(askPrice);
                 if (intAskPrice > curPrice) continue
-    
+
                 // 낮다면 거래를 체결한다.
                 for (let playerID in askList[askPrice]) {
                     let roomID = askList[askPrice][playerID];
@@ -66,17 +67,17 @@ class Game {
                     let socketID = playerInfo["socketID"];
                     cash += askVol * intAskPrice;
                     playerInfo["cash"] = String(cash);
-    
+
                     dbset(roomID, playerID, playerInfo);
                     socket.to(socketID).emit("askDone");
-    
+
                     delete playerID["askList"][askPrice];
                     delete askList[askPrice][playerID];
                 }
             }
             dbset("askList", askList);
         }
-    
+
         // 2-3. curPrice < prePrice면, bidPrice에서 curPrice보다 높은 호가를 처리한다.
         if (curPrice < prePrice) {
             let bidList = await dbget("bidList");
@@ -84,7 +85,7 @@ class Game {
             for (let bidPrice in bidList) {
                 let intBidPrice = Number(bidPrice);
                 if (intBidPrice < curPrice) continue
-    
+
                 // 높다면 거래를 체결한다.
                 for (let playerID in bidList[bidPrice]) {
                     let roomID = bidList[bidPrice][playerID];
@@ -94,17 +95,17 @@ class Game {
                     let socketID = playerInfo["socketID"];
                     coinVol += vol;
                     playerInfo["coinVol"] = String(coinVol);
-    
+
                     dbset(roomID, playerID, playerInfo);
                     socket.to(socketID).emit("bidDone");
-    
+
                     delete playerID["bidList"][bidPrice];
                     delete bidList[bidPrice][playerID];
                 }
             }
             dbset("bidList", bidList);
         }
-    
+
         // if (curPrice === prePrice) {
         //     let askList = await dbget("askList", String(curPrice));
         //     // 낮다면 거래를 체결한다.
@@ -116,18 +117,18 @@ class Game {
         //         let socketID = playerInfo["socketID"];
         //         cash += askVol * intAskPrice;
         //         playerInfo["cash"] = String(cash);
-    
+
         //         dbset(roomID, playerID, playerInfo);
         //         socket.to(socketID).emit("askDone");
-    
+
         //         delete playerID["askList"][askPrice];
         //         delete askList[playerID];
         //     }
         //     dbset("askList", String(curPrice), askList);
-    
+
         //     let bidList = await dbget("bidList", String(curPrice));
         //     // bidPrice가 curPrice보다 높은지 확인
-    
+
         //     // 높다면 거래를 체결한다.
         //     for (let playerID in bidList) {
         //         let roomID = bidList[playerID];
@@ -137,10 +138,10 @@ class Game {
         //         let socketID = playerInfo["socketID"];
         //         coinVol += bidVol;
         //         playerInfo["coinVol"] = String(coinVol);
-    
+
         //         dbset(roomID, playerID, playerInfo);
         //         socket.to(socketID).emit("bidDone");
-    
+
         //         delete playerID["bidList"][bidPrice];
         //         delete bidList[playerID];
         //     }
@@ -149,7 +150,7 @@ class Game {
     }
 
 
-    
+
 
 
 }
