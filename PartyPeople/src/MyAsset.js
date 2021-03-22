767facfdb2ddb6d4f60a8515955d80b21aea585f
Taeyang Oh <sunio00000@naver.com>
2021-03-22 16:38:31 +0900

    docs: missing files....ㅠ

--- PartyPeople/src/MyAsset.js
+++ PartyPeople/src/MyAsset.js
@@ -41,7 +41,7 @@ export default function MyAsset(props) {
                 props.requestSocket('MyAsset')
             }
             else {
-                props.socket.on('asset', (data)=> { //@ buyreq
+                props.socket.on('testAsset', (data)=> { //@ buyreq
                     console.log('자산을 갱신합니다.', data);
                     const currentCash = data.cash;
                     const currentAsset = data.asset;
