517e10d77c8ad4f76ecf1fa27667f8d36e1be551
Taeyang Oh <sunio00000@naver.com>
2021-03-22 16:16:54 +0900

    fix: server-side 컴파일 에러 해결 - testfile.js에 데이터를 안받고 있었음 - async 과정에서 인자를 전달하면 안됐음

--- GameDJ/gameDJ/testfile.js
+++ GameDJ/gameDJ/testfile.js
@@ -1,3 +1,5 @@
+import datas from "./dummy/dummyDatas4.js";
+const stockData = datas;
 import { dbset, dbget } from "./redis.js";
 
 class Test {
@@ -14,20 +16,22 @@ class Test {
   }
 
   async refresh(data) {
+    const { socket } = this;
     console.log(socket.id, "님의 자산이 갱신되었습니다.");
     socket.emit("testAsset", {
       cash: data.cash,
       asset: data.asset,
     });
   }
-
-  async testSendChart() {
-    let day = 0;
-    socket.emit("chart", stockData[day++]);
+  
+  async testSendChart(day) {
+    const { socket } = this;
+    socket.emit("chart", stockData[day]);
     console.log(socket.id, "님에게 [", day, "] 인덱스의 정보가 보내졌습니다.");
   }
 
   async testBuy(data) {
+    const { socket } = this;
     console.log(
       socket.id,
       "님의 [ 가격",
@@ -44,6 +48,7 @@ class Test {
   }
 
   async testSell(data) {
+    const { socket } = this;
     console.log(
       socket.id,
       "님의 [ 가격",
