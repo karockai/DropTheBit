60cc997be149717b402ad4bf9a17b91224998bf2
syeeuns <syeeuns@gmail.com>
2021-03-24 16:37:33 +0900

    updateSettings music 수정중

--- GameDJ/gameDJ/room.js
+++ GameDJ/gameDJ/room.js
@@ -98,7 +98,7 @@ class Room {
         const { socket } = this;
 
         const roomID = data.roomID;
-        let musciPath = '../PartyPeople/src/audios/music/' + data.musicName;
+        let musicPath = '../PartyPeople/src/audios/music/' + data.musicName;
         let musicTime = Math.round(getDuration(musicPath)['PromiseResult']); // 초 단위로 저장됨 (클라에서 파싱해서 사용)
         dbhset(roomID, music, data.musicName);
         dbhset(roomID, gameTime, musicTime);
