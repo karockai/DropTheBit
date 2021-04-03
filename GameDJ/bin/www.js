f93ee03fc785b912c1c26c59b6196213e0d9300e
karockai <75655052+karockai@users.noreply.github.com>
2021-04-03 16:20:42 +0900

    PublicRoomID : AAA~ -> EnjouPublicGame

--- GameDJ/bin/www.js
+++ GameDJ/bin/www.js
@@ -18,7 +18,7 @@ global.prePrice = 0;
 // for stress test
 global.playerStress = 0;
 // for 공방
-global.publicRoomID = 'AAAAAAAAAAAAAAA';
+global.publicRoomID = 'EnjoyPublicGame';
 const server = app.listen(process.env.PORT || 5000, process.env.IP, () => {
     console.log(`Server listening on port ${server.address().port}`);
 });
