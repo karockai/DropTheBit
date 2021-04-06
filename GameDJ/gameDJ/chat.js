4a7ca00b54fc156c393ac97cdc5510b47d345ca2
syeeuns <syeeuns@gmail.com>
2021-04-06 22:12:07 +0900

    chatting logic 수정

--- GameDJ/gameDJ/chat.js
+++ GameDJ/gameDJ/chat.js
@@ -7,7 +7,7 @@ class Chat {
     // data : {roomID : roomID, message:message, author : playerID}
     messageReq(data) {
         const { io, socket } = this;
-        let writer = roomList[roomID][socket.id]['playerID'];
+        let writer = roomList[data.roomID][socket.id]['playerID'];
         io.to(data.roomID).emit('update', {message : data.message, author : writer});
     };
 
