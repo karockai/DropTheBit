50a293549acab2bdc41fa603ebbd18727895c7c6
Taeyang Oh <sunio00000@naver.com>
2021-04-07 14:24:22 +0900

    fix: 로비 리더보드 소켓통신 방법 변경

--- PartyPeople/src/LeaderBoard.js
+++ PartyPeople/src/LeaderBoard.js
@@ -18,8 +18,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button, Grid, GridList} from '@material-ui/core';
-import {ExpBySymbol, parseWonToStr} from './parseMoney';
+import { Button, Grid, GridList } from '@material-ui/core';
+import { ExpBySymbol, parseWonToStr } from './parseMoney';
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -41,271 +41,141 @@ const useStyles = makeStyles((theme) => ({
         // minWidth: 650,
     },
     tableHead: {
-        fontSize:'1vw'
+        fontSize: '1vw',
     },
     tableBody: {
         background: '#212529',
     },
-    tableBodyText : {
+    tableBodyText: {
         fontSize: '0.7vw',
-        color:'white'
+        color: 'white',
     },
-    container : {
-        width:'100%',
+    container: {
+        width: '100%',
         // maxHeight: 440,
-    }
+    },
 }));
 
-export default function LeaderBoard(props) { //socket
+export default function LeaderBoard(props) {
+    //socket
     const classes = useStyles();
     const rows = props.leaderBoard;
-    const [board, setBoard] = useState({
-        
-    });
+    const [board, setBoard] = useState({});
 
-    useEffect(() => { 
+    useEffect(() => {
+        //Client
+        props.socket.emit('lobbyBoard', 'lobbyBoard Request.');
         props.socket.once('lobbyBoard', (gameResult) => {
-            if(gameResult) {
+            console.log('lobbyBoard', gameResult);
+            if (gameResult) {
                 setBoard(gameResult);
             }
-        })
+        });
+        //Server
+        // props.socket.on('lobbyBoard', (comment) => {
+        //     props.socket.emit('lobbyBoard', Object(gameResult));
+        // });
+        //Object 형식은 'leaderBoard' 와 같이.
+
     }, []);
 
     const showScore = (score) => {
         console.log(score);
-        let result = ''
-        if (score  == 1) result += '🥇 ';
-        if (score  == 2) result += '🥈 ';
-        if (score  == 3) result += '🥉 ';
+        let result = '';
+        if (score == 1) result += '🥇 ';
+        if (score == 2) result += '🥈 ';
+        if (score == 3) result += '🥉 ';
         else result += '  ';
-    
+
         result += String(score);
-        return (
-            <span>
-            {result}
-            </span>
-        );
-    }
-    
-    const showAsset = (asset) => {
-    
-        let result = asset -100000000;
-        let color = 'white';
-        if (result > 0) color= 'red';
-        if (result < 0) color= '#1e88e5';
-        return(
-            <TableCell style={{fontSize:'2vh', color:color}} align="right">
-                {
-                    ExpBySymbol(parseWonToStr(result))+' 원'
-                }
-            </TableCell>
-            
-        );
-    
+        return <span>{result}</span>;
     };
-    
+
+    const showAsset = (asset) => {
+        let result = asset - 100000000;
+        let color = 'white';
+        if (result > 0) color = 'red';
+        if (result < 0) color = '#1e88e5';
+        return (
+            <TableCell style={{ fontSize: '2vh', color: color }} align="right">
+                {ExpBySymbol(parseWonToStr(result)) + ' 원'}
+            </TableCell>
+        );
+    };
 
     return (
-        <GridList style={{height:'100%'}}>
-        <Grid container direction={'column'} alignItems={'center'} style={{width:'100%', height:'100%'}}>
-        <TableContainer id ="테이블 컨테이너" component={Paper} className={classes.container} >
-            <Table
-                stickyHeader 
-                style={{width:'100%'}}
-                className={classes.table}
-                aria-label="simple table"
+        <GridList style={{ height: '100%' }}>
+            <Grid
+                container
+                direction={'column'}
+                alignItems={'center'}
+                style={{ width: '100%', height: '100%' }}
             >
-                <TableHead>
-                    <TableRow className={classes.tableHead}>
-                        <TableCell className={classes.tableHead} align="center">순위</TableCell>
-                        <TableCell className={classes.tableHead} align="right">
-                            플레이어 네임
-                        </TableCell>
-                        <TableCell className={classes.tableHead} align="right">
-                            최종 수익
-                        </TableCell>
-                        {/* <TableCell align="right">Fat&nbsp;(g)</TableCell> */}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
+                {console.log(board) && <Paper>'게임이 진행되지 않았습니다.'</Paper>}
+                <TableContainer
+                    id="테이블 컨테이너"
+                    component={Paper}
+                    className={classes.container}
+                >
+                    <Table
+                        stickyHeader
+                        style={{ width: '100%' }}
+                        className={classes.table}
+                        aria-label="simple table"
+                    >
+                        <TableHead>
+                            <TableRow className={classes.tableHead}>
                                 <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'15%'}}
-                                    align="center"
+                                    className={classes.tableHead}
+                                    align="left"
                                 >
-                                {showScore(1)}
+                                    {Array.isArray(board) ? '순위': '아직 게임이 진행 되지 않았습니다.' }
                                 </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                            <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
                                 <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
+                                    className={classes.tableHead}
+                                    align="right"
                                 >
-                                {showScore(2)}
+                                    {Array.isArray(board) ? '플레이어 이름': '' }
                                 </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                            <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
                                 <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
+                                    className={classes.tableHead}
+                                    align="right"
                                 >
-                                {showScore(2)}
+                                    {Array.isArray(board) ? '최종 수익': '' }
                                 </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
+                                {/* <TableCell align="right">Fat&nbsp;(g)</TableCell> */}
                             </TableRow>
-                            <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                                                       <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                            <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                                                       <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                                                       <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                            <TableRow key={'playerID'} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'12%'}}
-                                    align="center"
-                                >
-                                {showScore(2)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {'playerID'}
-                                </TableCell>
-                                {
-                                    showAsset('123123')
-                                }
-                            </TableRow>
-                            
-                        {/* {board?.map((row, idx) => (
-                            <TableRow key={row.playerID} id ="테이블셀로우" className={classes.tableBody}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    className={classes.tableBodyText} 
-                                    style={{width:'15%'}}
-                                    align="center"
-                                >
-                                {showScore(idx+1)}
-                                </TableCell>
-                                <TableCell className={classes.tableBodyText}  align="right">
-                                    {row.playerID}
-                                </TableCell>
-                                {
-                                    showAsset(row.asset)
-                                }
-                            </TableRow>
-                        ))} */}
-                    </TableBody>
-            </Table>
-        </TableContainer>
-    </Grid>
-    </GridList>
-
+                        </TableHead>
+                        <TableBody>
+                            {Array.isArray(board) &&
+                                board.map((row, idx) => (
+                                    <TableRow
+                                        key={row.playerID}
+                                        id="테이블셀로우"
+                                        className={classes.tableBody}
+                                    >
+                                        <TableCell
+                                            component="th"
+                                            scope="row"
+                                            className={classes.tableBodyText}
+                                            style={{ width: '15%' }}
+                                            align="left"
+                                        >
+                                            {showScore(idx + 1)}
+                                        </TableCell>
+                                        <TableCell
+                                            className={classes.tableBodyText}
+                                            align="right"
+                                        >
+                                            {row.playerID}
+                                        </TableCell>
+                                        {showAsset(row.asset)}
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </GridList>
     );
 }
