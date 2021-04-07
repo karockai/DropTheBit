e6436ca99400b291c35d7036d59a77f079bf7c99
karockai <75655052+karockai@users.noreply.github.com>
2021-04-07 16:51:13 +0900

    gameMusic add

--- PartyPeople/src/MusicLeader.js
+++ PartyPeople/src/MusicLeader.js
@@ -24,6 +24,12 @@ import Without_Me from './audios/music/Without_Me.mp3';
 import StormRoad from './audios/music/질풍가도.mp3';
 import Beethven_Virus from './audios/music/Beethven_Virus.mp3';
 import The_Wight_to_Remain from './audios/music/The_Wight_to_Remain.mp3';
+import Gaza from './audios/music/Gaza.mp3';
+import Another_One_Bites_The_Dust from './audios/music/Another_One_Bites_The_Dust.mp3';
+import Can_You_Dig_It from './audios/music/Can_You_Dig_It.mp3';
+import Csikos_Post from './audios/music/Csikos_Post.mp3';
+import Dont_Stop_Me from './audios/music/날_막지마.mp3';
+
 import TestMusic from './audios/music/TestMusic.mp3';
 
 const useStyles = makeStyles((theme) => ({
@@ -63,7 +69,11 @@ export default function MusicLeader(props) {
         질풍가도: StormRoad,
         Beethven_Virus: Beethven_Virus,
         The_Wight_to_Remain: The_Wight_to_Remain,
-        Random_Music: 'Random_Music',
+        가즈아: Gaza,
+        Another_One_Bites_The_Dust: Another_One_Bites_The_Dust,
+        Can_You_Dig_It: Can_You_Dig_It,
+        우편마차: Csikos_Post,
+        날_막지마: Dont_Stop_Me,
         TestMusic: TestMusic,
     };
 
@@ -101,7 +111,7 @@ export default function MusicLeader(props) {
                         {/* {props.music} */}
                     </InputLabel>
                     <Select
-                    className={classes.select}
+                        className={classes.select}
                         inputProps={{
                             classes: {
                                 select: classes.select,
@@ -131,6 +141,16 @@ export default function MusicLeader(props) {
                         <MenuItem value={'The_Wight_to_Remain'}>
                             The_Wight_to_Remain
                         </MenuItem>
+                        <MenuItem value={'가즈아'}>가즈아</MenuItem>
+                        <MenuItem value={'Another_One_Bites_The_Dust'}>
+                            Another_One_Bites_The_Dust
+                        </MenuItem>
+                        <MenuItem value={'Can_You_Dig_It'}>
+                            Can_You_Dig_It
+                        </MenuItem>
+                        <MenuItem value={'우편마차'}>우편마차</MenuItem>
+                        <MenuItem value={'날_막지마'}>날_막지마</MenuItem>
+
                         <MenuItem value={'TestMusic'}>TestMusic</MenuItem>
                         {/* <>
               {props.musicList.map((music) => {
