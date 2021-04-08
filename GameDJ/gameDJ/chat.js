79eaccdf7e2e5f12bae4fe03bb713f8315086fbb
syeeuns <syeeuns@gmail.com>
2021-04-08 21:24:24 +0900

    test script 수정, chat 예외처리

--- GameDJ/gameDJ/chat.js
+++ GameDJ/gameDJ/chat.js
@@ -7,7 +7,10 @@ class Chat {
     // data : {roomID : roomID, message:message, author : playerID}
     messageReq(data) {
         const { io, socket } = this;
-        let writer = roomList[data.roomID][socket.id]['playerID'];
+        let writer = 0;
+        if (roomList[data.roomID][socket.id]['playerID']){
+            writer = roomList[data.roomID][socket.id]['playerID'];
+        }
         io.to(data.roomID).emit('update', {message : data.message, author : writer});
     };
 
