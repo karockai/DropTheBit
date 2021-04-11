6ee5f767599ddbd9e192b26bdd0b907c57442cce
Taeyang Oh <sunio00000@naver.com>
2021-04-12 02:11:26 +0900

    fix: 인게임 화면 아래로 밀리던 문제 해결

--- PartyPeople/src/ChatRoom.js
+++ PartyPeople/src/ChatRoom.js
@@ -206,7 +206,7 @@ export default function ChatRoom(props) {
             wrap={'nowrap'}
             
         > */}
-            <GridList className="채팅창그리드리스트" style={{ width: '100%', height:'85%'}} wrap={'wrap'}>
+            <GridList className="채팅창그리드리스트" style={{ width: '100%', height:'84%'}} wrap={'wrap'}>
                 <Grid
                     direction={'column'}
                     justify={'flex-start'}
