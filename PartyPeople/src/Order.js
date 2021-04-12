0dc43c36e94f2b55bbefdc8585770c9edacf5318
karockai <75655052+karockai@users.noreply.github.com>
2021-04-12 20:12:30 +0900

    order snackbar

--- PartyPeople/src/Order.js
+++ PartyPeople/src/Order.js
@@ -1,4 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Fade from '@material-ui/core/Fade';
+import Snackbar from '@material-ui/core/Snackbar';
+import {
+    ExpBySymbol,
+    parseWonToStr,
+    SplitByThree,
+    showProfit,
+} from './parseMoney';
 
 export default function Order(props) {
     const [orderReady, setReady] = useState(false);
@@ -46,12 +54,17 @@ export default function Order(props) {
     if (orderReady) {
         return (
             <>
-                <span style={{ color: 'white', fontSize: '2vw' }}>
-                    {orderType +
+                <Snackbar
+                    open={orderReady}
+                    TransitionComponent={Fade}
+                    message={
+                        orderType +
                         '주문 | 호가 : ' +
-                        orderPrice +
-                        '원 | 취소 : [C]'}
-                </span>
+                        SplitByThree(String(orderPrice)) +
+                        '원 | 취소 : [C]'
+                    }
+                    key={Fade}
+                />
             </>
         );
     } else {
